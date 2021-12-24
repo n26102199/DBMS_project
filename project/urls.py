17b2ad2629ef8b29239178ab9b20de484171c158
n26102199 <n26102199@gs.ncku.edu.tw>
2021-12-24 18:38:06 +0800

    basic button function ok!

--- project/urls.py
+++ project/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('select_from_where_demo', views.select_from_where_demo, name="select_from_where_demo"),
     ]
--- project/views.py
+++ project/views.py
@@ -17,7 +17,7 @@ def home(request):
     try:
         with connection.cursor() as cursor:
         # 執行sql語句，進行查詢
-            sql = 'SELECT * FROM TEST_TABLE'
+            sql = 'SELECT * FROM VENUE'
             cursor.execute(sql)
             # 獲取查詢結果
             result = cursor.fetchall()
@@ -28,3 +28,37 @@ def home(request):
         connection.close();
 
     return render(request, 'home.html', {'result': result})
+
+def select_from_where_demo(request):
+    config = {
+    'host':'127.0.0.1',
+    'port':3306,
+    'user':'root',
+    'password':'n26102199',
+    'db':'DBMS_project_server',
+    'charset':'utf8mb4',
+    'cursorclass':pymysql.cursors.DictCursor,
+    }
+    # Connect to the database
+    connection = pymysql.connect(**config)
+
+    temp = request.GET['selected form']
+    error_flag = 0
+    
+    try:
+        with connection.cursor() as cursor:
+        # 執行sql語句，進行查詢
+            sql = temp
+            cursor.execute(sql)
+            # 獲取查詢結果
+            results = cursor.fetchall()
+            print(results)
+            # 沒有設定預設自動提交，需要主動提交，以儲存所執行的語句
+            connection.commit()
+    except:
+        error_flag = 1
+        return render(request, 'select_from_where_demo.html', {'temp': temp,'error_flag': error_flag})
+    finally:
+        connection.close();
+
+    return render(request, 'select_from_where_demo.html', {'results': results, 'sql': sql, 'temp': temp, 'error_flag': error_flag})
